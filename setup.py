aab55599baed6c7213180b8f02b7dfd6605fa2c6
Filippo Castelli <filippocastelli42@gmail.com>
2019-03-05 20:27:07 +0100

    changing dir structure

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+
+# view filename
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "KEGGutils",
+    version = "0.0.1a",
+    author = "Filippo Castelli",
+    author_email = "filippocastelli42@gmail.com",
+    description = ("Simple utils to work with KEGG data on NetworkX"),
+    license = "Unlicense",
+    keywords = "KEGG networkx utils",
+    url = "https://github.com/filippocastelli/KEGGutils",
+    packages=['KEGGutils'],
+    install_requires=['networkx', 'requests'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: Unlicense",
+    ],
+)
