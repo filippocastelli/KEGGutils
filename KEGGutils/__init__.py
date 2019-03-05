92344f0c0fcb89919bd9e4af4205b14c4a6db033
Filippo Castelli <filippocastelli42@gmail.com>
2019-03-05 20:51:14 +0100

    init import fixed

--- KEGGutils/__init__.py
+++ KEGGutils/__init__.py
@@ -1,2 +1,2 @@
-from KEGGutils import (kegg_graph, get_organism_codes,kegg_url,
+from .KEGGutils import (kegg_graph, get_organism_codes,kegg_url,
                      get_nodetype_nodes, connected_components, kegg_infos)
