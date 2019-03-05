717b5e8eb188695013645e975d267f71aa4c9690
Filippo Castelli <filippocastelli42@gmail.com>
2019-03-05 19:47:59 +0100

    changed some names

--- test_script.py
+++ test_script.py
@@ -1,18 +1,15 @@
-from KGutils import (kegg_graph, get_organism_codes,kegg_url,
+from KEGGutils import (kegg_graph, get_organism_codes,kegg_url,
                      get_nodetype_nodes, connected_components, kegg_infos)
 
-from networkx.algorithms.bipartite import projected_graph
-
-import numpy as np
-import pandas as pd
-
 #%%
+print("The human genes vs diseases db is at", kegg_url("disease", "hsa"))
+
 KEGG = kegg_graph("disease", "hsa")
+
+subgraphs = connected_components(KEGG)
+
 
 genes = get_nodetype_nodes(KEGG, "hsa")
 diseases = get_nodetype_nodes(KEGG, "disease")
-#%%
-gene_net = projected_graph(KEGG,genes)
-disease_net = projected_graph(KEGG, diseases)
 
-subgraphs = connected_components(disease_net)
+kegg_infos('hsa:64109', verbose = True)
