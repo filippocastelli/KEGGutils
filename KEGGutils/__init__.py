ee2b47f169677edf0ac262c4a7d352ff8be51a07
Filippo Castelli <37574218+filippocastelli@users.noreply.github.com>
2019-03-11 00:07:56 +0100

    shame on me

--- KEGGutils/__init__.py
+++ KEGGutils/__init__.py
@@ -5,4 +5,4 @@ from .KEGGutils import (kegg_graph, get_organism_codes,kegg_url,
                      NotAKeggGraphError,MissingNodetypeError, NoProjectedError,
                      neighbor_graph, download_dir, delete_cached_files, linked_nodes)
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
