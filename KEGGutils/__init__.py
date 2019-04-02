1c8e347a80db8d333b4e58face28a146788a4382
Filippo Castelli <37574218+filippocastelli@users.noreply.github.com>
2019-04-02 22:12:17 +0200

    preparing for new dist

--- KEGGutils/__init__.py
+++ KEGGutils/__init__.py
@@ -34,4 +34,4 @@ from .KEGGpathway import KEGGpathway
 from .KEGGgraphs import (KEGGgraph,
                         KEGGlinkgraph,
                         KEGGchain)
-__version__ = "0.2.0"
+__version__ = "0.3.0"
